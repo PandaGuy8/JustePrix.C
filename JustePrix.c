c7c5f962a8c20eac71c6a177e54c6941dacc81a5
Andrea_Larghi <andrealarghi888@gmail.com>
2022-10-06 20:40:52 +0200

    Bug Fix

--- JustePrix.c
+++ JustePrix.c
@@ -9,30 +9,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 int main(void)
 {
-        int Input = 0;
+        int InputValue = 0;
         int Attempts = 15;
         int MaxValue = 50000;
-
+        int Input;
+ 
         srand(time(NULL));
 
         int bonPrix = rand()/(RAND_MAX/MaxValue);
        //printf("%d \n", bonPrix); //Debug only, prints the value to guess
-        printf("Entrez un chiffre entre 0 et %d\n", MaxValue);
-        
+        printf("Entrez un chiffre entier entre 0 et %d\n", MaxValue);
+
         while(Attempts != 0){
                 printf("Vous avez %d essais \n", Attempts);
-                scanf("%d", &Input);
+                
+                Input = scanf("%d", &InputValue);
 
-                if(Input < bonPrix){
+                if(Input == 0){
+
+                        printf("Ce n'est pas un entier\n");
+                        Attempts = Attempts + 1;
+                        return 1;
+                }
+
+                if(InputValue < bonPrix && Input != 0){
                         printf("C'est plus\n\n");
                 }
-                else if (Input > bonPrix){
+                else if (InputValue > bonPrix && Input != 0){
                         printf("C'est moins\n\n");
                 }
-                else{
+                else if(Input != 0){
                         printf("C'est juste !\n\n");
                         break;
                 }
@@ -41,7 +51,7 @@ int main(void)
         }
 
         if (Attempts == 0){
-                printf("C'était %d \n Vous avez perdu...\n\n", bonPrix);
+                printf("C'était %d \nVous avez perdu...\n\n", bonPrix);
         }
         //printf("%d \n", Input); //Debug only, prints the input value
 return 0;
